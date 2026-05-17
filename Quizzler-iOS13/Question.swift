6a4d751e426b6cf3288a59179e1b85a3373c3549
adel-daniel <adel.daniel.222@gmail.com>
2026-05-17 16:03:43 +0300

    Finish the app

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Adel Nabil on 17/05/2026.
+//  Copyright © 2026 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,80 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    
+    let questions: [Question] = [
+        Question(text: "A slug's blood is green.", answer: "True"),
+        Question(text: "Approximately one quarter of human bones are in the feet.", answer: "True"),
+        Question(text: "The total surface area of two human lungs is approximately 70 square metres.", answer: "True"),
+        Question(text: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", answer: "True"),
+        Question(text: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", answer: "False"),
+        Question(text: "It is illegal to pee in the Ocean in Portugal.", answer: "True"),
+        Question(text: "You can lead a cow down stairs but not up stairs.", answer: "False"),
+        Question(text: "Google was originally called 'Backrub'.", answer: "True"),
+        Question(text: "Buzz Aldrin's mother's maiden name was 'Moon'.", answer: "True"),
+        Question(text: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", answer: "False"),
+        Question(text: "No piece of square dry paper can be folded in half more than 7 times.", answer: "False"),
+        Question(text: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", answer: "True")
+        
+    ]
+    var currentQuestionIndex: Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateQuestion()
+        updateProgressBar()
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        updateProgressBar()
+                
+        let userAnswer = sender.currentTitle!
+        let currentAnswer = questions[currentQuestionIndex].answer
+        userAnswer == currentAnswer ? print("That's correct!") : print("Not quite.")
+        updateButtonColor(sender: sender, isCorrect: userAnswer == currentAnswer)
+        
+        currentQuestionIndex += 1
+        currentQuestionIndex %= questions.count
+        updateQuestion()
+    }
+    
+    
+    func updateQuestion() {
+        questionLabel.text = questions[currentQuestionIndex].text
+    }
+    
+    func updateButtonColor(sender: UIButton, isCorrect: Bool){
+        if (isCorrect) {
+            UIView.animate(withDuration: 0.1) {
+                sender.backgroundColor = UIColor.green
+            }
+        }else {
+            UIView.animate(withDuration: 0.1) {
+                sender.backgroundColor = UIColor.red
+            }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            UIView.animate(withDuration: 0.1) {
+                sender.backgroundColor = UIColor.clear
+            }
+        }
+        
+    }
+    
+    func updateProgressBar() {
+        UIView.animate(withDuration: 0.25) {
+            self.progressBar.progress = Float(self.currentQuestionIndex + 1) / Float(self.questions.count)
+        }
     }
 
-
+    
 }
 
